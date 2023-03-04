7c1f23bd212bbddee9f52a6d35e7b5f75f513107
Chintan6602~ <201901018@daiict.ac.in~>
2023-03-04 18:51:12 +0530

    Implemented main page

--- app/src/main/java/com/example/gaming_network/MainActivity.kt
+++ app/src/main/java/com/example/gaming_network/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.gaming_network
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+    private lateinit var spinner: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        spinner = findViewById(R.id.spinnerGame)
+
+        spinner.onItemSelectedListener = this
+
+
+        val arrayAdapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.Games,
+            android.R.layout.simple_spinner_item
+        )
+
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinner.adapter = arrayAdapter
+
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, view: View?, p2: Int, p3: Long) {
+        (view as TextView).setTextColor(Color.WHITE)
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+
     }
 }
