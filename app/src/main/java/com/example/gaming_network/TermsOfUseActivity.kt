6e7b6dd240c68312f0617bbd3a2b0ff6bc1b60cb
jeel <jeelfaldu90@gmail.com>
2023-03-20 19:31:55 +0530

    added toast to terms of use

--- app/src/main/java/com/example/gaming_network/TermsOfUseActivity.kt
+++ app/src/main/java/com/example/gaming_network/TermsOfUseActivity.kt
@@ -2,10 +2,13 @@ package com.example.gaming_network
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class TermsOfUseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_terms_of_use)
+
+        Toast.makeText(applicationContext,"Hi",Toast.LENGTH_SHORT).show()
     }
 }
